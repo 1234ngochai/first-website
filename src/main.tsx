0047e9ecc05d630469f3e79836f4abbd610b5d9c
Kei <keinguyen2212@gmail.com>
2025-11-01 23:01:00 +0700

    add sample react component

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+type Props = {
+  text?: string;
+}
+
+function ComponentA(props: Props) {
+  console.log("props", props);
+  return <button type="button" onClick={() => alert(`clicked ${props.text}`)}>{props.text}</button>
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    Hello world
+    {[...Array(10)].map((_, idx) => (
+      <ComponentA text={`Click me ${idx + 1}`} />
+    ))}
+    {/* <App /> */}
   </StrictMode>,
 )
